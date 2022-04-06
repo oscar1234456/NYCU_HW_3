fd4471e9cd2558e93ae853d9fc5004037de1bd42
oscar12344456 <chen.oscar@hotmail.com>
2022-04-06 17:42:47 +0800

    [Add]add poly basis

--- data_generator.py
+++ data_generator.py
@@ -2,12 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from numpy import log as ln
 
+
 # m: mean
 # s: variance
 def random_data_generator(m, s):
     U = np.random.rand()
     V = np.random.rand()
-    X = np.sqrt((-2 * ln(U)))*np.cos(2*np.pi*V)
+    X = np.sqrt((-2 * ln(U))) * np.cos(2 * np.pi * V)
     # X is drawn from normal distribution N(0, 1)
     # E[ax+b] = a * E[x]+b ; var[ax+b] = a^2 * var[x]
     # Therefore, we need to let a^2 be s => a = sqrt(s)
@@ -16,14 +17,23 @@ def random_data_generator(m, s):
     return convert_X
 
 
-
-
+# n: basis number
+# a: variance
+# w: parameters
+def poly_data_generator(n, a, w):
+    x = np.random.uniform(-1, 1)
+    noise = random_data_generator(0, a)
+    phi = np.array([np.power(x, i) for i in range(n)])
+    # w.T @ phi is scalar
+    y = w.T @ phi + noise
+    return x, y
 
 
 if __name__ == "__main__":
-    samples = list()
-    for i in range(20000):
-         samples.append(random_data_generator(5,1))
-    plt.hist(samples, 50)
-    plt.show()
-
+    # samples = list()
+    # for i in range(20000):
+    #      samples.append(random_data_generator(5,1))
+    # plt.hist(samples, 50)
+    # plt.show()
+    x, y = poly_data_generator(2, 10, np.array([2, 5]))
+    print(f"x:{x}, y:{y}")
