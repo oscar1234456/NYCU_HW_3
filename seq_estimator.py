31e28ca7ad6c77334a28e0d9c143148a41cfec5a
陳泰元 <chentaiyuan@chentaiyuans-Mac-mini.local>
2022-04-07 11:56:04 +0800

    [Add]complete seq estimatetor

--- seq_estimator.py
+++ seq_estimator.py
@@ -2,4 +2,34 @@ import data_generator
 
 
 def seq_estimator(m, s):
-    new_point = data_generator.random_data_generator(m, s)
+    eps = 1e-4
+    max_iter = 100000
+    n = 0
+    mean_now = 0
+    variance_now = 0
+    M2 = 0
+
+    print(f"Data point source function: N({m}, {s})")
+
+    for _ in range(max_iter):
+        new_point = data_generator.random_data_generator(m, s)
+        print(f"Add data point: {new_point}")
+        n += 1
+        mean_new = mean_now + ((new_point - mean_now) / n)
+        M2 += (new_point - mean_now) * (new_point - mean_new)
+        variance_new = (0 if n == 1 else (M2 / (n - 1)))  # Sample variance [unbiased]
+        print(f"Mean = {mean_new}   Variance = {variance_new}")
+
+        if (abs(mean_new - mean_now) <= eps) and (abs(variance_new - variance_now) <= eps):
+            print("___converge___ (early stop)")
+            break
+
+        mean_now = mean_new
+        variance_now = variance_new
+
+    print(f"total runs: {n}")
+
+
+if __name__ == "__main__":
+    # seq_estimator(3.0,5.0)
+    seq_estimator(241.0, 8.0)
